0b3fa4ef15b0a5b1feb2b807977785f0b166e137
dsavelev <dsavelev@fun-box.ru>
2014-02-12 13:40:15 +0400

    Dirty hack

--- lib/rchardet/codingstatemachine.rb
+++ lib/rchardet/codingstatemachine.rb
@@ -14,12 +14,12 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
@@ -44,7 +44,7 @@ module CharDet
     def next_state(c)
       # for each byte we get its class
       # if it is first byte, we also get byte length
-      b = c[0]
+      b = c[0].to_i
       byteCls = @model['classTable'][b]
       if @currentState == EStart
         @currentBytePos = 0
